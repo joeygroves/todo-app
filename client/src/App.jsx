2ef494a5dabb799471a6b1def57718c0a3b6c6b0
Joey Groves <link_groves@yahoo.co.uk>
2024-04-24 16:27:07 +0100

    made task component have rounded corners

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </div>
 
         <div className="flex justify-center">
-          <div className="flex p-4 w-11/12 bg-grey">
+          <div className="flex p-4 w-11/12 bg-grey rounded-sm">
             <input type="checkbox" className="py-4 px-2"/>
             <p className="px-2 text-md w-10/12">Pick up dry cleaning</p>
             <button className="bg-blue hover:bg-slate-400 transition duration-500 rounded-full w-7 h-7 mr-3 float-right"></button>
