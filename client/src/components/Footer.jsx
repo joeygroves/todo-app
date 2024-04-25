b2c3824d182f79d90230951a11ee16f687721a87
Joey Groves <link_groves@yahoo.co.uk>
2024-04-25 14:27:45 +0100

    created a footer component and refactored App.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="pt-3 pb-8">
+            <p className="text-center text-gray-500 text-xs">
+            &copy; 2024 <a href="https://www.github.com/joeygroves" className="hover:text-orange transition duration-500">Joey Groves</a>. All rights reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
