c1787e4053d339b03707439bb686de8856a9ed21
Joey Groves <link_groves@yahoo.co.uk>
2024-04-25 15:01:48 +0100

    CSS change to make app UI smaller

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,7 @@ const App = (props) => {
   return (
     <div className="bg-teal flex flex-col h-screen justify-center items-center pt-10">
 
-      <div className="box-content h-screen w-3/5 max-w-screen-lg rounded-3xl drop-shadow-md bg-white">
+      <div className="box-content h-screen w-3/5 max-w-screen-sm rounded-3xl drop-shadow-md bg-white">
         <h1 className="text-black text-4xl font-semibold p-12">To-Do List</h1>
 
         <TodosForm 
