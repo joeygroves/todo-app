548e9fec989e980982fc9af7ae0b8a83c67a9999
Joey Groves <link_groves@yahoo.co.uk>
2024-04-29 18:29:31 +0100

    created a simple web server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const http = require('http')
+
+let todos = [
+  {
+    id: 1,
+    content: "Pick up dry cleaning",
+    complete: false
+  },
+  {
+    id: 2,
+    content: "Do the dishes",
+    complete: true
+  },
+  {
+    id: 3,
+    content: "Finish lectures",
+    complete: false
+  },
+]
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(todos))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
