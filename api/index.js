56dc3f3cc8a2eb3bf92277108427de0df9216d63
Joey Groves <link_groves@yahoo.co.uk>
2024-04-29 18:35:13 +0100

    created an express server

--- api/index.js
+++ api/index.js
@@ -1,27 +1,33 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let todos = [
-  {
-    id: 1,
-    content: "Pick up dry cleaning",
-    complete: false
-  },
-  {
-    id: 2,
-    content: "Do the dishes",
-    complete: true
-  },
-  {
-    id: 3,
-    content: "Finish lectures",
-    complete: false
-  },
-]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(todos))
+    {
+      id: 1,
+      content: "Pick up dry cleaning",
+      complete: false
+    },
+    {
+      id: 2,
+      content: "Do the dishes",
+      complete: true
+    },
+    {
+      id: 3,
+      content: "Finish lectures",
+      complete: false
+    },
+  ]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/todos', (request, response) => {
+  response.json(todos)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
