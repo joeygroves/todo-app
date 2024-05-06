06234d12d984bf0e610ad1c4a8bb49369e273fc2
Joey Groves <link_groves@yahoo.co.uk>
2024-05-06 18:54:36 +0100

    connected to mongodb

---
+++ api/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://babushka:${password}@cluster0.het17of.mongodb.net/todoApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const todoSchema = new mongoose.Schema({
+  content: String,
+  complete: Boolean,
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+
+const todo = new Todo({
+  content: 'HTML is easy',
+  important: true,
+})
+
+todo.save().then(result => {
+  console.log('todo saved!')
+  mongoose.connection.close()
+})
